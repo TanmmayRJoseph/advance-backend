69f01a9e20e38a9ee961d88a5226e76d3e0a9c0f
Tanmmay R Joseph <tanmmayrj@gmail.com>
2026-04-03 22:26:04 +0530

    implemented rate limiting

--- 1-auth-jwt-refresh/src/server.ts
+++ 1-auth-jwt-refresh/src/server.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectToDatabase } from "./utils/dbConnect";
 import userRoutes from "./routes/user.routes";
+
+import { rateLimit } from 'express-rate-limit';// rate limiter
 dotenv.config();
 
 // Connect to MongoDB
@@ -10,9 +12,20 @@ connectToDatabase();
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 const PORT = process.env.PORT || 3000;
 
